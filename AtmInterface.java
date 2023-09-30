d63563e68f07a1e165b8d8ec3aa7537ec5d18f26
SanketSurude <surudesanket@gmail.com>
2023-09-30 14:11:26 +0530

    Create AtmInterface.java

---
+++ AtmInterface.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class AtmInterface {
+    public static int takeIntegerInput(int limit) {
+        int input = 0;
+        boolean flag = false;
+
+        while (!flag) {
+            try {
+                Scanner sc = new Scanner(System.in);
+                input = sc.nextInt();
+                flag = true;
+
+                if (flag && input > limit || input < 1) {
+                    System.out.println("Choose the number between 1 to " + limit);
+                    flag = false;
+                }
+            } catch (Exception e) {
+                System.out.println("Enter only integer value");
+                flag = false;
+            }
+        }
+        return input;
+    }
+    public static void main(String[] args) {
+        
+    }
+}
