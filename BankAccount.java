96c148396180993bb6ad3e6117970ed6546acacc
SanketSurude <surudesanket@gmail.com>
2023-09-30 20:51:06 +0530

    Update BankAccount.java

--- BankAccount.java
+++ BankAccount.java
@@ -22,6 +22,30 @@ class BankAccount {
         System.out.println("\nRegistration completed..kindly login");
     }
 
+    public boolean login() {
+        boolean isLogin = fase;
+        Scanner sc = new Scanner(System.in);
+        while (!isLogin) {
+            System.out.print("\nEnter Your Username - ");
+            String Username = sc.nextLine();
+            if (Username.equals(userName)) {
+                while (!isLogin) {
+                    System.out.print("\nEnter Your Password - ");
+                    String Password = sc.nextLine();
+                    if (Password.equals(password)) {
+                        System.out.print("\nLogin successful!!");
+                        isLogin = true;
+                    } else {
+                        System.out.println("\nIncorrect Password");
+                    }
+                }
+            } else {
+                System.out.println("\nUsername not found");
+            }
+        }
+        return isLogin;
+    }
+
     public void withdraw() {
         System.out.print("\nEnter amount to withdraw - ");
         Scanner sc = new Scanner(System.in);
@@ -60,6 +84,43 @@ class BankAccount {
         }
     }
 
-    
+    public void transfer() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("\nEnter Recipient's Name - ");
+        String recipient = sc.nextLine();
+        System.out.print("\nEnter amount to transfer - ");
+        float amount = sc.nextFloat();
+        try {
+            if (balance >= amount) {
+                if (amount <= 50000f) {
+                    transactions++;
+                    balance -= amount;
+                    System.out.println("\nSuccessfully Transferred to " + recipient);
+                    String str = amount + " Rs transferred to " + recipient + "\n";
+                    transactionHistory = transactionHistory.concat(str);
+                } else {
+                    System.out.println("\nSorry...Limit is 50000.00");
+                }
+            } else {
+                System.out.println("\nInsufficient Balance");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void checkBalance() {
+        System.out.println("\n" + balance + " Rs");
+    }
+
+    public void transHistory() {
+        if (transactions == 0) {
+            System.out.println("\nEmpty");
+        } else {
+            System.out.println("\n" + transactionHistory);
+        }
+    }
+}
+
 }
 
